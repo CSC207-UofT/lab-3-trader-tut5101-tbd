20feb704679b3ee213e9fba97d9f1a5d1a241119
Qiyi <qiyi.zhang@mail.utoronto.ca>
2021-09-28 07:07:11 +0800

    1

---
+++ src/main/java/AnimeGirl.java
@@ -0,0 +1,21 @@
+public class AnimeGirl implements Domesticatable, Tradable{
+    public AnimeGirl() {
+
+    }
+
+    @Override
+    public String sound() {
+        return "Biubiubiu";
+    }
+
+    @Override
+    public int getPrice() {
+        return 80;
+    }
+
+    @Override
+    public String toString(){
+        String id = super.toString();
+        return id + "kwawiiiiiii";
+    }
+}
